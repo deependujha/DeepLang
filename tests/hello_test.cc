497426f65e57ad66b9cc909e324907d2b3a7328c
Deependu Jha <deependujha21@gmail.com>
2024-12-01 11:30:29 +0530

    update

--- tests/hello_test.cc
+++ tests/hello_test.cc
@@ -1,11 +1,24 @@
 #include <gtest/gtest.h>
 #include "llvm/ADT/APFloat.h"
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/LLVMContext.h"
+#include "llvm/IR/Module.h"
 
 class SimpleTest : public testing::Test {};
 
+static void InitializeModule() {
+    using namespace llvm;
+    // Open a new context and module.
+    auto TheContext = std::make_unique<LLVMContext>();
+    auto TheModule = std::make_unique<Module>("my cool jit", *TheContext);
+
+    // Create a new builder for the module.
+    auto Builder = std::make_unique<IRBuilder<>>(*TheContext);
+}
+
 // Demonstrate some basic assertions.
 TEST_F(SimpleTest, BasicAssertions) {
-    llvm::fltSemantics;
+    InitializeModule();
     // Expect two strings not to be equal.
     EXPECT_STRNE("hello", "world");
     // Expect equality.
